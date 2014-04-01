cbf32a564088944ef55c398b7c14b29e39b6bee3
jophis <joseph.longmoore@gmail.com>
2014-04-01 16:17:40 -0400

    added additional paths

--- crm.rb
+++ crm.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
 
 get '/' do 
+	@crm_app_name = "Joseph's world changing CRM"
 	erb :index
 end
+
+get '/contacts' do
+
+end
+
+get '/contacts/new' do
+
+end
+
+get 'contacts/:id' do
+
+end
+
+get 'contacts/:id/edit' do
+
+end
+
