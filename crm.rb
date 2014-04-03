925b3690174b30a225bdcf6823052b7ef24f03d7
jophis <joseph.longmoore@gmail.com>
2014-04-03 17:12:06 -0400

    can now edit a contact

--- crm.rb
+++ crm.rb
@@ -64,5 +64,22 @@ post '/contacts' do
 	redirect to ('/contacts/new')
 end
 
+put "/contacts/:id" do
+	@contact = @@rolodex.find(params[:id].to_i)
+	if @contact
+		@contact.first_name= params[:first_name]
+		@contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.notes = params[:notes]
+
+    redirect to ("/contacts")
+
+  else
+  	raise Sinatra::NotFound
+  end
+end 
+
+
+
 
 
